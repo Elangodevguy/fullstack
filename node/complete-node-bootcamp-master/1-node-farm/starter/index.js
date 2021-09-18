549a412a15b7598c57483f0a783c36bb06c9c769
Elangovan Palanisamy <elango@Elangovans-MacBook-Pro.local>
2021-09-18 09:49:39 +0530

    11 creating a simple web server

--- node/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ node/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const http = require('http');
+
+// ------------------ Files ------------------------
 
 // Synchronous blocking--------------------------------
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -7,20 +10,29 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut)
 // console.log('File written!!!!!!!!')
 
-// Asynchronous, non blocking way to write
-fs.readFile('./txt/start.txt', (err, data1) => {
-    if(err) {
-        return console.log(err)
-    }
-    console.log(data1)
-    fs.readFile(`./txt/${data1}.txt`, (err, data2) => {
-        console.log(data2)
-        fs.readFile('./txt/append.txt', (err, data3) => {
-            const textOut = `${data2}\n${data3}`;
-            console.log(textOut)
-            fs.writeFile('./txt/final.txt',textOut, err => {
-                console.log('its done')
-            })
-        })
-    })
-})
+// Asynchronous, non blocking way to write---------------
+// fs.readFile('./txt/start.txt', (err, data1) => {
+//     if(err) {
+//         return console.log(err)
+//     }
+//     console.log(data1)
+//     fs.readFile(`./txt/${data1}.txt`, (err, data2) => {
+//         console.log(data2)
+//         fs.readFile('./txt/append.txt', (err, data3) => {
+//             const textOut = `${data2}\n${data3}`;
+//             console.log(textOut)
+//             fs.writeFile('./txt/final.txt',textOut, err => {
+//                 console.log('its done')
+//             })
+//         })
+//     })
+// })
+
+// ----------------------- Server ----------------------------------------------
+const server = http.createServer((req, res) => {
+    res.end("hello from the server");
+});
+
+server.listen(8000, '127.0.0.1', err => {
+    console.log("Server is listening on port number:" + "8000")
+} )
