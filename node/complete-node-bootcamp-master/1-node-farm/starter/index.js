715058b79a1e50cecc1f7498efa18996e63e6af0
Elangovan Palanisamy <elango@Elangovans-MacBook-Pro.local>
2021-09-18 08:27:59 +0530

    8 reading and writting files

--- node/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ node/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
-const hello = 'Hello world!!!!!!!!'
 
-console.log(hello)
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+
+const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()}`
+
+fs.writeFileSync('./txt/output.txt', textOut)
+
+console.log('File written!!!!!!!!')
