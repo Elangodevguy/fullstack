46e49daa9553807e6bff351a5bfb9d82405319a1
Elangovan Palanisamy <elango@Elangovans-MacBook-Pro.local>
2021-09-18 09:25:07 +0530

    10 reading and writting files async

--- node/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ node/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// Synchronous blocking--------------------------------
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()}`
 
-const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File written!!!!!!!!')
 
-fs.writeFileSync('./txt/output.txt', textOut)
-
-console.log('File written!!!!!!!!')
+// Asynchronous, non blocking way to write
+fs.readFile('./txt/start.txt', (err, data1) => {
+    if(err) {
+        return console.log(err)
+    }
+    console.log(data1)
+    fs.readFile(`./txt/${data1}.txt`, (err, data2) => {
+        console.log(data2)
+        fs.readFile('./txt/append.txt', (err, data3) => {
+            const textOut = `${data2}\n${data3}`;
+            console.log(textOut)
+            fs.writeFile('./txt/final.txt',textOut, err => {
+                console.log('its done')
+            })
+        })
+    })
+})
