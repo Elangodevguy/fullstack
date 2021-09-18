6cdb735cb4821ff5475fc9f2d9ab092c931061b7
Elangovan Palanisamy <elango@Elangovans-MacBook-Pro.local>
2021-09-18 20:11:36 +0530

    22 setting up prettier in vs code

--- node/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ node/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-const slugify = require("slugify");
-const replaceTemplate = require("./modules/replaceTemplate");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // ------------------ Files ------------------------
 
@@ -32,11 +32,20 @@ const replaceTemplate = require("./modules/replaceTemplate");
 // })
 
 // ----------------------- Server ----------------------------------------------
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 const slugified = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugified);
@@ -44,39 +53,43 @@ const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
   console.log(req.url);
   // const pathname = req.url;
-
-  if (pathname === "/" || pathname === "/overview") {
-    // overview
+  // overview
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
-      "Content-type": "text/html",
+      'Content-type': 'text/html',
     });
-    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
-  } else if (pathname === "/product") {
-    // products
+
+    // product
+  } else if (pathname === '/product') {
     res.writeHead(200, {
-      "Content-type": "text/html",
+      'Content-type': 'text/html',
     });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
-  } else if (pathname === "/api") {
+
     // api
+  } else if (pathname === '/api') {
     res.writeHead(200, {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     });
     res.end(data);
-  } else {
+
     // page not found
+  } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "Elango",
+      'Content-type': 'text/html',
+      'my-own-header': 'Elango',
     });
-    res.end("<h1>Page not found</h1>");
+    res.end('<h1>Page not found</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", (err) => {
-  console.log("Server is listening on port number:" + "8000");
+server.listen(8000, '127.0.0.1', (err) => {
+  console.log('Server is listening on port number:' + '8000');
 });
