d9161e67d4f4ecef78c028d4d69954925952f0ff
Elangovan Palanisamy <elango@Elangovans-MacBook-Pro.local>
2021-09-27 23:30:46 +0530

    4-hoisting_in_js

--- javascript/akshay/namaste_js/index.js
+++ javascript/akshay/namaste_js/index.js
@@ -1,8 +1,25 @@
-var n = 2;
+// 3 How js code is executed
+// var n = 2;
 
-function square(x) {
-  return x * x;
+// function square(x) {
+//   return x * x;
+// }
+
+// var square2 = square(2);
+// var square4 = square(4);
+
+// 4 Hoisting
+debugger;
+var x = 5;
+aaa(x);
+function aaa() {
+  console.log(x);
+  function elango() {
+    console.log(x + x);
+  }
+  elango();
 }
 
-var square2 = square(2);
-var square4 = square(4);
+var aaab = () => {
+  console.log("arrow");
+};
