14164c41d903619f93684d07e04b3ce3fb800432
Elangovan Palanisamy <elango@Elangovans-MacBook-Pro.local>
2021-09-28 06:55:26 +0530

    5-how_fn_works_in_js

--- javascript/akshay/namaste_js/index.js
+++ javascript/akshay/namaste_js/index.js
@@ -23,3 +23,22 @@
 // var aaab = () => {
 //   console.log("arrow");
 // };
+// 5 How functions work in js
+debugger;
+var x = 10;
+var y = {
+  x: 200,
+};
+a();
+b();
+
+function a() {
+  console.log(this.x);
+}
+function b() {
+  var x = 1000;
+  console.log(x);
+}
+
+var binded = a.bind(y);
+binded();
